28d91cd49847e3add3af57511b8b664fe07451a8
Chayka Toxcik <Chayka Toxcik>
2016-03-05 16:53:06 +0200

    added timer

---
+++ test.js
@@ -0,0 +1,41 @@
+var date = 0;
+var timer = null;
+function startTimer() {
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("stopButton").disabled = false;
+    document.getElementById("clearButton").disabled = true;
+    document.getElementById("sendButton").disabled = true;
+    timer = setInterval(function() {
+        date += 1;
+        var millis = date % 1000;
+        var sec = Math.floor(date / 1000);
+        sec = sec % 60;
+        var min = Math.floor(date / 60000);
+        var hours = Math.floor(date / 36000000);
+        if (Math.floor(sec/10) == 0) sec = "0" + sec;
+        if (Math.floor(min/10) == 0) min = "0" + min;
+        if (Math.floor(hours/10) == 0) hours = "0" + hours;
+        //if (Math.floor(hours/10) > 0) hours = "0" + hours;
+        document.getElementById("timer").value = hours + ":" + min + ":" + sec + "." + millis;
+
+    }, 1);
+}
+
+function stopTimer() {
+    if(timer != null)
+    window.clearInterval(timer);
+    document.getElementById("startButton").disabled = false;
+    document.getElementById("clearButton").disabled = false;
+    document.getElementById("sendButton").disabled = false;
+}
+
+function clearTimer() {
+    document.getElementById("startButton").disabled = false;
+    document.getElementById("stopButton").disabled = true;
+    document.getElementById("clearButton").disabled = true;
+    document.getElementById("timer").value = 0;
+}
+
+function sendResult() {
+    document.getElementById("sendButton").disabled = true;
+}
