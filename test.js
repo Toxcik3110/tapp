d54a94e71190a127964e2dae29513bd14dda60b6
Chayka Toxcik <Chayka Toxcik>
2016-03-06 03:40:13 +0200

    merged third page

--- test.js
+++ test.js
@@ -2,7 +2,10 @@ var date1 = null;
 var date2 = null;
 var date = 0;
 var timer = null;
-var penalty = false;
+var penalty = true;
+var selectedState = "";
+var selectedTeam = "";
+var transtitionCount = 0;
 function startTimer() {
     document.getElementById("startButton").disabled = true;
     document.getElementById("stopButton").disabled = false;
@@ -63,15 +66,100 @@ function reduceCounter(index) {
 }
 
 function changePage() {
+    if (transtitionCount == 0) {
+        selectedState = document.getElementById("stages").options[document.getElementById("stages").selectedIndex].text;
+        document.getElementById("team").value =
+        document.getElementById("teams").options[document.getElementById("teams").selectedIndex].text;
+    }
+    transtitionCount += 1;
     if(!penalty) {
         penalty = !penalty;
+        document.getElementById("third").style.display = "none";
         document.getElementsByClassName("bottom")[0].innerHTML = "К таймеру";
+        document.getElementById("header").innerHTML = selectedState;
         document.getElementById("second").style.display = "flex";
         document.getElementById("first").style.display = "none";
+        document.getElementById("exit").style.display = "block";
     } else {
         penalty = !penalty;
+        document.getElementById("third").style.display = "none";
         document.getElementsByClassName("bottom")[0].innerHTML = "К штрафам";
+        document.getElementById("header").innerHTML = selectedState;
         document.getElementById("second").style.display = "none";
         document.getElementById("first").style.display = "flex";
+        document.getElementById("exit").style.display = "block";
     }
+}
+
+function exit() {
+    transtitionCount = 0;
+    document.getElementById("third").style.display = "flex";
+    document.getElementById("first").style.display = "none";
+    document.getElementById("second").style.display = "none";
+    document.getElementsByClassName("bottom")[0].innerHTML = "Судить";
+    document.getElementById("header").innerHTML = "Приветствуем в TAPP!";
+    document.getElementById("exit").style.display = "none";
+}
+
+function getData(str) {
+    var xhttp;
+    var boo = 0;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState = 4 && xhttp.status == 200 && boo < 2) {
+            boo = boo + 1;
+            console.log(JSON.parse(xhttp.responseText));
+            console.log(JSON.parse(xhttp.responseText).length);
+            var teams = JSON.parse(xhttp.responseText);
+            for(var i = 0; i < teams.length; i++) {
+                var option = document.createElement("option");
+                option.text = teams[i].name;
+                option.value= teams[i].name;
+                document.getElementById("teams").add(option);
+            }
+            getData1("getStages");
+        }
+    };
+    xhttp.open("GET", "http://localhost:1337/" + str, true);
+    xhttp.send();
+}
+function getData1(str) {
+    var xhttp;
+    var boo = 0;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState = 4 && xhttp.status == 200 && boo < 2) {
+            boo = boo + 1;
+            console.log(JSON.parse(xhttp.responseText));
+            console.log(JSON.parse(xhttp.responseText).length);
+            var stages = JSON.parse(xhttp.responseText);
+            for(var i = 0; i < stages.length; i++) {
+                var option = document.createElement("option");
+                option.text = stages[i].name;
+                option.value= stages[i].id;
+                document.getElementById("stages").add(option);
+            }
+        }
+    };
+    xhttp.open("GET", "http://localhost:1337/" + str, true);
+    xhttp.send();
+}
+
+function addTeam(name) {
+    var xhttp;
+    var boo = 0;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState = 4 && xhttp.status == 200 && boo < 2) {
+            boo = boo + 1;
+            if(xhttp.responseText == "OK") {
+                var option = document.createElement("option");
+                option.text = name;
+                option.value= name;
+                document.getElementById("teams").add(option);
+            }
+        }
+    };
+    xhttp.open("GET", "http://localhost:1337/addTeam " + name, true);
+    xhttp.send();
 }
